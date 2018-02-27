cfc3622881c04eaa72a26227fb4769120df68b22
carol-solivos <carol.solivos@gmail.com>
2018-02-26 22:50:32 -0400

    data 1.0

---
+++ src/data/data.js
@@ -0,0 +1,287 @@
+// datos de artículos en venta
+let dataArticle = {
+	'Tecnologia': {
+		'notebooks': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'smartphones': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'accesorios': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		}
+	},
+	'Moda': {
+		'blusas': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'pantalones': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'calzado': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		}
+	},
+	'Hogar': {
+		'comedores': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'living': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'cocina': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		}
+	},
+	'Deportes': {
+		'futbol': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'ciclismo': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		},
+		'fitness': {
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			},
+			'Correa de Estiramiento para Yoga': {
+				'desc': 'La correa de estiramiento perfecta para principiantes e incluso mejor para usuarios experimentados.',
+				'new': true,
+				'precio': '$21.990',
+				'disponible': true,
+				'img': 'https://ripleycl.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fimagenes-sellers-mercado-ripley%2FImagenes-MIRAKL%2F2018%2F02%2FMPM00000457735-1-F.jpg?w=750&h=555&ch=Width&auto=format&cs=strip&q=60&ixlib=js-1.1.0&s=44d9eaedaf9de1b9b9af74cd4aaf369d'
+			}
+		}
+	}
+}
